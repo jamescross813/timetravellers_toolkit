b6fde049d7283194cc03e773a9d141f99acfead6
jamescross813 <crossjames813@gmail.com>
2024-12-20 04:35:57 -0600

    readable date time

--- time_travelers_toolkit.py
+++ time_travelers_toolkit.py
@@ -5,4 +5,5 @@ from random import choice
 import custom_module
 
 current_date_and_time = dt.datetime.now().time()
-print(current_date_and_time)
+print(current_date_and_time.strftime('%l:%M%p %Z on %b %d, %Y'))
+
