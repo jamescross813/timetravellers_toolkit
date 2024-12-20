f6cb737392a711cf4c29e7dad9e5104023315214
jamescross813 <crossjames813@gmail.com>
2024-12-20 05:54:24 -0600

    format

--- custom_module.py
+++ custom_module.py
@@ -1,2 +1,2 @@
 def generate_time_travel_message(year, destination, cost):
-    return print(f'In the {year}, I landed in {destination}. The trip cost {cost}')
+    return print(f'In the {year}, I landed in {destination}. The trip cost me {cost}!')
