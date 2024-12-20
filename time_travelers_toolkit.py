16c2e63d8ab5db0a92abcfc7fbacdbd5285d8a3e
jamescross813 <crossjames813@gmail.com>
2024-12-20 05:38:42 -0600

    random_year generator

--- time_travelers_toolkit.py
+++ time_travelers_toolkit.py
@@ -6,10 +6,12 @@ import custom_module
 
 current_year = dt.date.today().year
 current_date_and_time = dt.datetime.now().time()
-print(current_date_and_time.strftime('%l:%M%p %Z on %b %d, %Y'))
+random_year = randint(int(current_year), 10000)
 
 base_cost = Decimal('8.73')
 
 cost_multiplier = abs(current_year-random_year)
 
 final_cost = base_cost * cost_multiplier
+
+
